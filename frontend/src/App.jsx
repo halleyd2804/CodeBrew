51cb7a11df4bc350b5afcc588000f09335b7da97
JoeyHammoth <137293223+JoeyHammoth@users.noreply.github.com>
2025-04-24 15:16:06 +1000

    Cleaned up everything

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+// Root JSX file where all the components are collated within
 
-function App() {
+import './App.css'
+import {createRoot} from "react-dom/client";
+import {StrictMode} from "react";
+
+const App = function () {
 
   return (
-    <>
-      <div className="test motion-scale-in-0">Hello</div>
-    </>
-  )
+      <StrictMode>
+        <div className="test motion-scale-in-0">Hello</div>
+      </StrictMode>
+  );
 }
 
-export default App
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
