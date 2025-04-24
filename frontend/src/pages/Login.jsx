bad719bc92c05f27c59bc2cef103786d0a6e3be7
Shannon Yap <kxfiiclub@gmail.com>
2025-04-25 09:09:11 +1000

    Added css & jsx files for Login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+// Login page of web app
+// Accessible from sidebar (???)
+
+import "./Login.css";
+import React from 'react';
+
+const Login = function () {
+    return(
+        <div className="loginFields">
+            
+            {/*<input type="text" id="username" placeholder="Username..." /> <<< Need to add functionality*/}
+
+
+            {/* <button onClick={}>Submit</button> <<< Unfinished*/}
+
+        </div>
+    )
+}
+
+export default Login;
